5a09abf60fdf00062e14bbe807753ae74d8163e9
PC903 <missbahodirova@gmail.com>
2023-02-10 16:06:46 +0500

    commit

---
+++ app/src/main/java/uz/itschool/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package uz.itschool.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var main = findViewById<ImageView>(R.id.main)
+    }
+}
